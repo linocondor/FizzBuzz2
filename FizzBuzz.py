9684755d9e5517e2047af0a20b50204d25dad4a5
Lino Cóndor <lino.condor.123@gmail.com>
2021-06-14 12:10:21 -0500

    first commit

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+print("This is a simple program that print Fizz or Buzz or FizzBuzz")
+
+for number in range(1,101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
+
---
+++ README.md
@@ -0,0 +1,6 @@
+#Fizz Buzz
+This is a simple program that follows this rules:
+    1. Print number from 1 to 100
+    2. When number is divisible by 5 print Buzz
+    3. When number is divisible by 3 print Fizz
+    4. When number is divisible by 3 and 5 print FizzBuzz
